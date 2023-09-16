43ea3da23d8e9dd6d3a0ceae3a1d145e03bc0b1a
zhouyuan <zhouyuan10@staff.weibo.com>
2023-09-16 19:57:24 +0800

    修改配置

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/jenkins-vite-test/', // 基础路径
-  plugins: [vue()],
-})
+
+const env = process.env.ENV || ''; // 环境
+
+
+export default ({command, mode}) => {
+
+  console.log('command', command, mode, env)
+  return defineConfig({
+    base: env ? `/jenkins-vite-test/${env}/` : '/jenkins-vite-test/', // 基础路径
+    plugins: [vue()],
+  })
+}
